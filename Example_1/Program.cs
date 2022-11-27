b39d75ca9bef32abaa5c006fd2ad6fa2a632e73b
Dimitriizz <dmitriiqtr@gmail.com>
2022-11-27 16:33:10 +0400

    added Example 1

---
+++ Example_1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначиное число ");
+
+int namber = int.Parse(Console.ReadLine());
+
+int secondnNumber = namber % 100 / 10;
+
+Console.WriteLine(secondnNumber);
